48e15837c37641f7f86b686a232dea66b9ba8252
Ryan Rawlinson <Ryan.Rawlinson@blueyonder.com>
2025-07-24 12:22:41 -0500

    Initial Commit

---
+++ Stylish/StylishApp.swift
@@ -0,0 +1,17 @@
+//
+//  StylishApp.swift
+//  Stylish
+//
+//  Created by Ryan Rawlinson on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct StylishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
